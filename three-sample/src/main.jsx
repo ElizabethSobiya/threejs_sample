54e0cd595dc9ea6af157d66362e7fc0529a897f7
elizabeth1098 <esobiya@futuryze.com>
2023-02-20 20:48:59 +0530

    resize

--- three-sample/src/main.jsx
+++ three-sample/src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import * as THREE from 'three';
+import './style.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+//scene
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color:'#00ff83',
+})
+
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+//sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+//light
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 10)
+scene.add(light)
+
+//camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 100)
+camera.position.z = 20
+scene.add(camera)
+
+
+//renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGL1Renderer({canvas})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+
+//resize
+
+window.addEventListener('resize', () =>{
+console.log(window.innerWidth);
+sizes.width = window.innerWidth;
+sizes.height = window.innerHeight;
+} )
